2751090a692181da1dcd56c66d5a2e33b445c085
Matheus Sousa <matheusousaa@gmail.com>
2019-07-16 21:38:27 -0300

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,7 @@ class User extends Model {
 	const SECRET = "HcodePhp7_Secret";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSuccess";
 
 	public static function getFromSession() {
 		
@@ -309,6 +310,28 @@ class User extends Model {
 	public static function clearError() {
 
 		$_SESSION[User::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess($msg) {
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess() {
+
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess() {
+
+		$_SESSION[User::SUCCESS] = NULL;
 
 	}
 
